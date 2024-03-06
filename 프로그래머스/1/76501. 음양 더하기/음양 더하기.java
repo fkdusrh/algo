ad80c00fa022459f13b72ee0ecc8a9ba0066c54f
rayeon <fkdusrh@naver.com>
2024-03-06 22:08:12 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for(int i=0;i<absolutes.length;i++){
+            int number = absolutes[i];
+            if(!signs[i])
+                number*=-1;
+
+            answer+=number;
+        }
+        return answer;
+    }
+}

2c457574b43bcafe7f9ac4961ab40474e9bd50ef
rayeon <fkdusrh@naver.com>
2024-04-11 20:56:57 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.74 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -21,11 +21,10 @@ class Solution {
         //1. 대문자 -> 소문자
         new_id= new_id.toLowerCase();
         //2. 소문자, 숫자, -, _ , . 나머지는 제외
-        new_id = new_id.replaceAll("[^a-z0-9-\\.\\_]","");
-        //3. .. -> . 치환
-        new_id = new_id.replaceAll("\\.{2,}",".");
-        //4. 처음과 끝 . 제거
-        new_id = new_id.replaceAll("^\\.|\\.$","");
+        new_id = new_id.replaceAll("[^a-z0-9-\\.\\_]","")
+            .replaceAll("\\.{2,}",".") //3.  ..연속 -> . 로 치환
+            .replaceAll("^\\.|\\.$","");//4. 처음과 끝 . 제거
+   
         //5. 빈 문자열이면 a 대입
         if(new_id.length()==0)
             new_id="a";
@@ -34,7 +33,8 @@ class Solution {
             new_id = new_id.substring(0,15);
         //6.2 만약 뒤가 .이면 제거
         new_id = new_id.replaceAll("\\.$","");
-        //7. 2자 이하라면, 마지막 문자를 3자 이상 될때까지 반복해서 붙이기
+        //7. 2자 이하라면, 마지막 문자를 3자 이상 될때까지 마지막 문자 반복해서 붙이기
+        
         if(new_id.length()<3){            
             StringBuilder sb = new StringBuilder(new_id);
             char last=sb.charAt(sb.length()-1); 

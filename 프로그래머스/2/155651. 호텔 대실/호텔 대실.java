02c4b3aba053b04d410b9c643273566d4a7f3701
rayeon <fkdusrh@naver.com>
2024-06-19 23:27:46 +0900

    [level 2] Title: 호텔 대실, Time: 4.71 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,68 +1,61 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = 0;
         List<Book> books = convertToMinute(book_time);
-        
+
+        // Book의 startTime을 기준으로 오름차순 정렬
         Collections.sort(books, new Comparator<Book>() {
             @Override
             public int compare(Book b1, Book b2) {
                 return Integer.compare(b1.getStartTime(), b2.getStartTime());
             }
         });
-    
-        
-        List<List<Book>> rooms = new LinkedList<>(); 
-        boolean success = false;
 
-        for(Book book:books){//예약 건수
-            success = false;
-            for(List<Book> roomTimeTable:rooms){ // 방 타임 테이블
-                if (roomTimeTable.isEmpty() || 
-                    roomTimeTable.get(roomTimeTable.size() - 1).endTime + 10 <= book.startTime) {
-                    roomTimeTable.add(book);
-                    success=true;
-                    break;
-                }
-                
-                if(success)
-                    break;
+        // 종료 시간을 기준으로 오름차순 정렬되는 PriorityQueue
+        PriorityQueue<Integer> rooms = new PriorityQueue<>();
+
+        for (Book book : books) {
+            // 가장 빨리 끝나는 방의 종료 시간을 가져옴
+            if (!rooms.isEmpty() && rooms.peek() + 10 <= book.startTime) {
+                // 기존 방에 현재 예약을 추가할 수 있는 경우
+                rooms.poll(); // 해당 방의 종료 시간을 제거
             }
-            if(!success){
-                rooms.add(new LinkedList<Book>());
-                rooms.get(rooms.size()-1).add(book);
-             }
+            // 새로운 종료 시간을 추가
+            rooms.add(book.endTime);
         }
-        
+
+        // 필요한 방의 수는 PriorityQueue에 남아 있는 요소의 수와 같음
         return rooms.size();
     }
-    
-    public static List<Book> convertToMinute(String[][] book_time){
+
+    public static List<Book> convertToMinute(String[][] book_time) {
         List<Book> books = new LinkedList<>();
-        for(String[] book : book_time){
-            int startMin=calculate(book[0]);
+        for (String[] book : book_time) {
+            int startMin = calculate(book[0]);
             int endMin = calculate(book[1]);
-            books.add(new Book(startMin,endMin));
+            books.add(new Book(startMin, endMin));
         }
         return books;
     }
-    public static int calculate(String time){
-        int hour = Integer.parseInt(time.substring(0,2));
-        int minute = Integer.parseInt(time.substring(3,5));
-        
-        return hour*60 + minute;
+
+    public static int calculate(String time) {
+        int hour = Integer.parseInt(time.substring(0, 2));
+        int minute = Integer.parseInt(time.substring(3, 5));
+        return hour * 60 + minute;
     }
 }
 
-class Book{
+class Book {
     int startTime;
     int endTime;
-    public Book(int start, int end){
+
+    public Book(int start, int end) {
         this.startTime = start;
         this.endTime = end;
     }
-    
-    public int getStartTime(){
+
+    public int getStartTime() {
         return this.startTime;
     }
 }

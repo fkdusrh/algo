f1ebb767e065a4f279a11836957445189083ca68
rayeon <fkdusrh@naver.com>
2026-02-02 16:33:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-
-        int copyX=x;
-        int sum=0;
-        for(int i=0;copyX>0;i++){
-            sum+=copyX%10;
-            copyX/=10;
+        int sum = 0;
+        for(char c:String.valueOf(x).toCharArray()){
+            sum += c-'0';
         }
-        if(x%sum==0)
-            return true;
-
-        return false;
+        return x % sum == 0;
     }
 }

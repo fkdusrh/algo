ee049743180552131f3515b7ae60dd0da0a984e0
rayeon <fkdusrh@naver.com>
2026-02-02 15:55:05 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -1,26 +1,6 @@
 class Solution {
-     public int solution(String s) {
-        int answer = 0;
-
-        int start = s.length() - 1;
-        int sum=0;
-        int cnt=1;
-
-        for (int i = 0; start - i >= 0; i++) {
-
-            if( s.charAt(start - i) =='-'){
-                sum*=-1;
-                break;
-            }
-            if( s.charAt(start - i) =='+'){
-                break;
-            }
-
-            int num = s.charAt(start - i) - '0' ;
-            sum+=(num*cnt);
-
-            cnt*=10;
-        }
-        return sum;
+    public int solution(String s) {
+        return Integer.parseInt(s);
+        
     }
 }

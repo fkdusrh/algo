88db0f9c06de77cc5416c2c57b5f01e94f27b980
rayeon <fkdusrh@naver.com>
2026-02-02 17:34:55 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.09 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,25 +1,9 @@
 class Solution {
-    public static long solution(long n) {
-        long sum = 1;
-        int cnt = 0;
-        long temp = n;
-        
-        for(long div = 2 ;div*div<= temp ;div++){
-            cnt = 0;
-            while(n%div==0){
-                n /= div; 
-                cnt++; 
-            }
-            if(cnt == 0)
-                continue;
-            if(cnt % 2 != 0) 
-                return -1;
-            for(int i=0;i<cnt/2;i++){ 
-                sum *= div;
-            }
-        }
-        if(n != 1)
-            return -1;
-        return (sum+1) * (sum + 1);
+    public long solution(long n) {
+        long answer = 0;
+        long root = (long)Math.sqrt(n);
+        if(root * root == n)
+            return (root+1) * (root+1);
+        return -1;
     }
 }

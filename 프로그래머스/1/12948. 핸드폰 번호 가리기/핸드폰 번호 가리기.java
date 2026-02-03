a4fc2617fc9ac20fe15357953e7cec5bec6dc3cc
rayeon <fkdusrh@naver.com>
2026-02-03 23:00:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,15 +1,11 @@
 class Solution {
-    public static String solution(String phone_number) {
-        StringBuilder sb = new StringBuilder();
-        String star;
-        String number;
-        for(int i=0;i<phone_number.length()-4;i++){
-            sb.append("*");
+    public String solution(String phone_number) {
+        char[] chars = phone_number.toCharArray();
+        
+        for(int i=0;i<phone_number.length() -4;i++){
+            chars[i] = '*';
         }
-
-       
-        sb.append(phone_number.substring(phone_number.length()-4,phone_number.length()));
-
-        return sb.toString();
+        
+        return new String(chars);
     }
 }

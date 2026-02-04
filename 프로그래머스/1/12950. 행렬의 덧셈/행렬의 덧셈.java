9b0212d020f6e7ed37e1ed6023df6c228cacdd50
rayeon <fkdusrh@naver.com>
2026-02-04 19:35:47 +0900

    [level 1] Title: 행렬의 덧셈, Time: 3.71 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -4,10 +4,10 @@ class Solution {
 
         for(int i=0;i<arr1.length;i++){
             for(int j=0;j<arr1[0].length;j++){
-                arr[i][j] = arr1[i][j] + arr2[i][j];
+                arr1[i][j] += arr2[i][j];
             }
         }
         
-        return arr;
+        return arr1;
     }
 }

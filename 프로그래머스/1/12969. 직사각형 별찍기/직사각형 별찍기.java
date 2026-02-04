177f94e79d24bbf8ce945463688ab2a59eb8c13b
rayeon <fkdusrh@naver.com>
2026-02-04 19:45:26 +0900

    [level 1] Title: 직사각형 별찍기, Time: 158.76 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(); // 가로 열
-        int b = sc.nextInt();// 세로 행
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
         
         StringBuilder sb = new StringBuilder(a*b+a);
         
         for(int i=0;i<b;i++){
             for(int j= 0;j<a;j++){
-                sb.append("*");
+                sb.append('*');
             }
-            sb.append("\n");
+            sb.append('\n');
         }
         
         System.out.println(sb.toString());

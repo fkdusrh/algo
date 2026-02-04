c89d054dc385a2f2e50ec0ac57268828ac90d084
rayeon <fkdusrh@naver.com>
2026-02-04 17:18:14 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.76 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,11 +1,12 @@
 class Solution {
     public String solution(int n) {
-        String[] arr = {"수","박"};
-        StringBuilder sb = new StringBuilder();
-        for(int i=1;i<=n;i++){
-            sb.append(arr[(i+1)%2]);
+        StringBuilder sb = new StringBuilder(n);
+        for(int i=0;i<n/2;i++){
+            sb.append("수박");
         }
-        String answer = sb.toString();
-        return answer;
+        if(n%2 != 0){
+            sb.append("수");
+        }
+        return sb.toString();
     }
 }

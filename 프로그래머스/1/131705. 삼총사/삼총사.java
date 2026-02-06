7445e779707ecc8345c373a56d4ada4aaf48ffb5
rayeon <fkdusrh@naver.com>
2026-02-06 16:14:39 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,45 +1,24 @@
-import java.util.*;
-class Solution {
-    
-    
-    public static int answerCnt = 0 ;
- 
-    public  int solution(int[] number) {
-        int answer = 0;
-        int[] comb = new int[3];
-      
-        combination(number,comb,0,0);
-        answer = answerCnt;
-        return answer;
+class Solution {// 조합
+    public int solution(int[] number) {
+        int[] team = new int[3];
+        return combination(number, team, 0, 0, 0) ;
     }
-
-    public  void combination(int[] number, int[] comb, int turn, int cnt){
-
-        if(cnt>2){
-            checkTripleZero(comb);
-            return;
+    
+    //매개변수 (원본 배열, 삼총사 멤버, 현재 멤버 인원,순회 시작 지점, 그룹 개수)
+    public int combination(int number[], int[] team, int memberCnt, int start, int groupCnt){
+        if(memberCnt == 3){
+            int sum = 0;
+            for(int n : team)
+                sum+=n;
+            
+            return sum==0?++groupCnt:groupCnt;
         }
-
-        if(turn >= number.length){
-            return;
+        
+        for(int i = start;i<number.length;i++){
+            team[memberCnt] = number[i];
+            groupCnt = combination(number, team, memberCnt+1, i+1,groupCnt);
         }
-
-        comb[cnt] = number[turn];
-
-        combination(number, comb, turn+1, cnt+1);
-
-        combination(number, comb, turn+1, cnt);
-    }
-
-    private static void checkTripleZero(int[] comb) {
-        int sum = 0;
-
-        for(int a : comb){
-            sum += a;
-        }
-        if(sum == 0){
-            System.out.println(Arrays.toString(comb));
-            answerCnt++;
-        }
+        
+        return groupCnt;
     }
 }

5eb6ed8f1edfc95526772831b2bd7951a01050d2
rayeon <fkdusrh@naver.com>
2026-02-06 17:46:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,24 +1,30 @@
-import java.util.*;
-
 class Solution {
-     public  int solution(int n) {
+    public int solution(int n) {
         int answer = 0;
-        Stack<Integer>  stack = new Stack<>();
-
-        while(n>=3){
-            stack.add(n%3);
-            n/=3;
+        StringBuilder sb = new StringBuilder();
+        while(n>0){
+            sb.append(n % 3);
+            n /= 3;
         }
-        stack.add(n);
-        //System.out.println(Arrays.toString(stack.toArray()));
-        int product = 1;
-
-        while(!stack.isEmpty()){
-            answer += (product * stack.pop());
-            product*=3;
-
+        
+        String s = new String(sb);
+        int size = s.length();
+        int num = 1;
+        
+        for(int i=0;i<s.length();i++){
+            answer += (num * (s.charAt(size-i-1)-'0'));
+            num*=3;
         }
-
+        
         return answer;
     }
 }
+
+/*
+
+1. n % 3 -> sb.append -> new String(sb) -> s.charAt() - '0'
+2.  for(i ; < arr.length; i*=3)
+    1) sum += ( * i)
+}
+
+*/

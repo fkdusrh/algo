e516626616b53d2472c5719d1c4356744e847f31
rayeon <fkdusrh@naver.com>
2026-02-06 16:35:50 +0900

    [level 1] Title: 예산, Time: 1.49 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,19 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
-        int answer = 0;
         Arrays.sort(d);
-        System.out.println(Arrays.toString(d));
-        int sum=0;
-        int cnt=0;
+        int sum = 0, cnt=0;
+        
         for(int i=0;i<d.length;i++){
-
-            if(sum+d[i]>budget)
-                return cnt;
-
+            if(sum + d[i] > budget)
+                break;
             sum+=d[i];
             cnt++;
         }
+        
         return cnt;
     }
 }

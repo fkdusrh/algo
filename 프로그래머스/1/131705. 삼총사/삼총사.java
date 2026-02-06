e7781d9f90bdea4e0ef7c96a4bb40773880a578b
rayeon <fkdusrh@naver.com>
2026-02-06 16:19:50 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,22 +1,16 @@
 class Solution {// 조합
     public int solution(int[] number) {
-        int[] team = new int[3];
-        return combination(number, team, 0, 0, 0) ;
+        return combination(number, 0, 0, 0, 0) ;
     }
     
     //매개변수 (원본 배열, 삼총사 멤버, 현재 멤버 인원,순회 시작 지점, 그룹 개수)
-    public int combination(int number[], int[] team, int memberCnt, int start, int groupCnt){
+    public int combination(int number[], int start, int sum, int memberCnt, int groupCnt){
         if(memberCnt == 3){
-            int sum = 0;
-            for(int n : team)
-                sum+=n;
-            
             return sum==0?++groupCnt:groupCnt;
         }
         
         for(int i = start;i<number.length;i++){
-            team[memberCnt] = number[i];
-            groupCnt = combination(number, team, memberCnt+1, i+1,groupCnt);
+            groupCnt = combination(number, i+1,sum+number[i], memberCnt+1, groupCnt);
         }
         
         return groupCnt;

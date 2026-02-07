0e2417e838ab8a27d7ffebdd9386198184174116
rayeon <fkdusrh@naver.com>
2026-02-07 19:50:19 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,6 +1,38 @@
 class Solution {
     public int solution(String dartResult) {
-        int answer = 0;
-        return answer;
+        int[] nums = new int[3];
+        int strIdx = -1;
+        
+        for(int i=0;i<dartResult.length();i++){
+            char c = dartResult.charAt(i);
+            
+            if(c-'0' >=0 && c-'0'<=9){
+                ++ strIdx;
+                if(c == '1' && dartResult.charAt(i+1) =='0'){
+                    nums[strIdx] = 10;
+                    i += 1;
+                }else{
+                   nums[strIdx] = c-'0';
+                }
+            }else if(c=='S'){
+                //x
+            }else if(c == 'D'){
+                nums[strIdx] *= nums[strIdx];
+            }else if(c=='T'){
+                nums[strIdx] *= (nums[strIdx] * nums[strIdx] );
+            }else if(c =='*'){ 
+                nums[strIdx] <<= 1;
+                if(strIdx > 0)
+                    nums[strIdx-1] <<= 1;
+            }else if(c == '#'){
+                nums[strIdx] *= -1;
+            }
+        }
+        
+        int sum = 0;
+        for(int n: nums){
+            sum+=n;
+        }
+        return sum;
     }
 }

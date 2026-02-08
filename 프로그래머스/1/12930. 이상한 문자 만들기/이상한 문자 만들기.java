56a49889aa562043fffc88ba416946248fdef17c
rayeon <fkdusrh@naver.com>
2026-02-08 19:55:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.11 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,39 +1,28 @@
-import java.util.*;
-
 class Solution {
-   public  String solution(String s) {
-
-        System.out.println((int) 'a');
-        String answer = "";
-        StringTokenizer st = new StringTokenizer(s);
-        int change = 'A';
-        int turn = 0;
+    public String solution(String s) {
+        boolean even = true;
+        int cnt = 0;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
-                sb.append(" ");
-                turn = 0;
+        
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+        
+            if(c == ' '){
+                cnt=0;
+                sb.append(' ');
                 continue;
             }
-            if (s.charAt(i) >= (int) 'a') {
-                if (turn % 2 == 0)
-                    sb.append((char) (s.charAt(i) - (int) 'a' + (int) 'A'));
-                else {
-                    sb.append((char) s.charAt(i));
-                }
-                turn++;
-            }else{//대문자
-                if(turn%2==0){
-                    sb.append((char) s.charAt(i));
-
-                }else{
-                    sb.append((char) (s.charAt(i) - (int) 'A' + (int) 'a'));
-                }
-                turn++;
+            
+            
+            if(cnt % 2 == 0){
+                sb.append(Character.toUpperCase(c));
+            }else{
+                sb.append(Character.toLowerCase(c));
             }
-
-
+            
+            cnt++;
+            
         }
-        return sb.toString();
+        return new String(sb);
     }
 }

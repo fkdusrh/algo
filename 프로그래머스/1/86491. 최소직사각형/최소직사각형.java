0d573a5e319590a2d02b9dc544e34daff07f134b
rayeon <fkdusrh@naver.com>
2026-02-09 17:27:31 +0900

    [level 1] Title: 최소직사각형, Time: 2.09 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,27 +1,22 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
+        int maxA = 0;
+        int maxB = 0;
+        
         for(int i=0;i<sizes.length;i++){
-            if(sizes[i][0]<sizes[i][1]){
+            if(sizes[i][0] < sizes[i][1]){
                 int tmp = sizes[i][0];
                 sizes[i][0] = sizes[i][1];
-                sizes[i][1]=tmp;
-                System.out.println("change!");
+                sizes[i][1] =tmp;
             }
         }
         
-        for(int i=0;i<sizes.length;i++)
-          System.out.println(Arrays.toString(sizes[i]));
-        int rowMax=0,colMax=0;
         for(int i=0;i<sizes.length;i++){
-            if(rowMax<sizes[i][0]){
-                rowMax=sizes[i][0];
-            }
-            if(colMax<sizes[i][1]){
-                colMax=sizes[i][1];
-            }
+            maxA = Math.max(sizes[i][0], maxA);
+            maxB = Math.max(sizes[i][1], maxB);
         }
-        return rowMax*colMax;
+        
+        return maxA * maxB;
     }
 }

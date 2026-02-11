c17e90d4316fd62a321be934ba0c71647a0a4819
rayeon <fkdusrh@naver.com>
2026-02-11 21:09:42 +0900

    [level 1] Title: 시저 암호, Time: 1.55 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,40 +1,20 @@
-import java.util.*;
 class Solution {
     public String solution(String s, int n) {
-        
-        String answer = "";
         StringBuilder sb = new StringBuilder();
-        System.out.println((int)'z');//-25
-        int alphabetCnt=26;
-        n%=alphabetCnt;
+        int alphaCnt = 'Z'-'A'+1;
         
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             
-            if(c==' '){
-                sb.append(" ");
-                continue;
+            if(c != ' ') {
+                char start = c >='A' && c<='Z'?'A':'a';
+                int idx = c+n - start;
+                
+                idx %= alphaCnt;
+                c = (char) (start + idx);
             }
             
-            //소문자
-            if(c >= 'a'){
-                if(c + n > 'z'){
-                   sb.append((char)((c+n-(int)'a') % alphabetCnt+'a'));
-                    continue;
-                }
-                 sb.append((char)(c+n));
-                continue;
-            }
-            
-            //대문자
-             if(c >= 'A'){
-                if(c + n > 'Z'){
-                   sb.append((char)((c+n-(int)'A') % alphabetCnt+'A'));
-                    continue;
-                }
-                 sb.append((char)(c+n));
-                continue;
-            }
+            sb.append(c);            
         }
         return sb.toString();
     }

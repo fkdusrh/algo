70361d9adbf404bf74ee9f7d3dc99c583de4a528
rayeon <fkdusrh@naver.com>
2026-02-11 21:35:44 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,42 +1,15 @@
 class Solution {
-//윤년 : 2월 29일이 있는 날.
-/*
-1-31
-2-29
-3-31
-4-30
-5-31
-6-30
-7-31
-8-31
-9-30
-10-31
-11-30
-12-31
-
-*/
     public String solution(int a, int b) {
-        int[] monthsDay = {31,29,31,30,31,30,31,31,30,31,30,31};
-        int sum =0;
-        for(int i=0;i<a-1;i++){
-            sum+=monthsDay[i];
-        }
-        sum+=b;
+        String[] dayOfWeek = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+        int[] dayOfMonth = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int sumDay = 0;
         
-        int day = sum%7;
-        if(day==0)
-            return "THU";
-        if(day==1)
-            return "FRI";
-        if(day==2)
-            return "SAT";
-        if(day==3)
-            return "SUN";
-        if(day==4)
-            return "MON";
-        if(day==5)
-            return "TUE";
-
-            return "WED";
+        for(int i=1;i<a;i++){
+            sumDay += dayOfMonth[i-1];
+        }
+                                                                                   sumDay += b;              
+        sumDay %= 7;
+        
+        return dayOfWeek[sumDay];
     }
 }

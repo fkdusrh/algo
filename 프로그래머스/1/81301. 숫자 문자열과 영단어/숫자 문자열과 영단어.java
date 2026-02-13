e4bb60f69d8eb45bb42f8304f3335156ec6f31d1
rayeon <fkdusrh@naver.com>
2026-02-13 18:38:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.39 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,32 +1,13 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-          int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        StringBuilder alpabet = new StringBuilder();
-        String[]  NUMBER = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-
-        for(int i=0;i<s.length();i++){
-
-            char c = s.charAt(i);
-
-            if(c>='0' && c<='9'){
-                sb.append(c);
-                continue;
-            }
-
-            alpabet.append(c);
-
-            for(int j=0;j<NUMBER.length;j++){
-                if(alpabet.toString().equals(NUMBER[j])){
-                    System.out.println("나는 숫자"+j+alpabet.toString());
-                    alpabet.setLength(0);
-                    sb.append(j);
-
-                    break;
-                }
-            }
-
+        
+        String[] NUMBER = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        
+        for(int i=0;i<10;i++){
+            s = s.replaceAll(NUMBER[i],Integer.toString(i));
         }
-        return Integer.parseInt(sb.toString());
+        
+        return Integer.valueOf(s);
     }
 }

bbd079203ab90bdc8264a8528112fc6c5cb26526
rayeon <fkdusrh@naver.com>
2026-02-14 19:49:20 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 94.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int sum = 0;
+        
+        
+        while(n >= a) {
+            sum += n / a *b ;
+            n = (n / a) * b + n%a;
+        }
+        
+        return sum;
+    }
+}
+
+
+//7:23-

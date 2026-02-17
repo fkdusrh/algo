48c1f28f5f7c7f365da578a0ccfbc8cd122b76a8
rayeon <fkdusrh@naver.com>
2026-02-17 20:33:13 +0900

    [level 1] Title: K번째수, Time: 1.12 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,25 +2,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
-        for(int i=0;i<commands.length;i++){//0,1,2
-            int[] copyArr = Arrays.copyOf(array,array.length);
-
-            for(int j=commands[i][0]-1;j<commands[i][1];j++){
-                System.out.println("j: "+j);
-
-                for(int k=j+1;k<commands[i][1];k++){
-                    if(copyArr[j]>copyArr[k]){
-                        int tmp = copyArr[k];
-                        copyArr[k]=copyArr[j];
-                        copyArr[j]=tmp;
-                    }
-                }
-
-            }
-            answer[i]=copyArr[commands[i][0]-1+commands[i][2]-1];
+        
+        for(int idx=0;idx<commands.length;idx++){
+            int i = commands[idx][0];
+            int j = commands[idx][1];
+            int k = commands[idx][2];
+            
+            int[] arr = Arrays.copyOfRange(array,i-1,j);
+            
+            Arrays.sort(arr);
+            answer[idx] = arr[k-1];
         }
-
+        
         return answer;
     }
 }

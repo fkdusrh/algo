7cc0483f61c4d291fb731a1b9c824245ef9a00e3
rayeon <fkdusrh@naver.com>
2026-02-19 22:47:31 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.22 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,52 +1,57 @@
-import java.util.*;
 class Solution {
     public String solution(int[] numbers, String hand) {
-        String answer = "";
         StringBuilder sb = new StringBuilder();
-        int currentLeft = 10;
-        int currentRight = 12;
-        int zero = 11;
-        for(int i=0;i<numbers.length;i++){
-            int currentNum = numbers[i];
-            //1,4,7 은 무조건 L
-            if(currentNum == 1 || currentNum == 4 || currentNum == 7){
-                sb.append("L");
-                currentLeft = currentNum;
-                continue;
+        int leftPosition = 10;
+        int zeroPosition = 11;
+        int rightPosition = 12;
+        boolean right = hand.equals("right") ? true:false;
+        
+        for(int number : numbers){
+            if(number % 3 == 1){
+                leftPosition = number;
+                sb.append('L');
             }
-                //3,6,9 는 무조건 R
-            if(currentNum == 3 || currentNum == 6 || currentNum == 9){
-                sb.append("R");
-                currentRight = currentNum;
-                continue;
+            else if(number == 3 || number == 6 || number == 9){
+                rightPosition = number;
+                sb.append('R');
             }
-            //차이가 작은 쪽으로
-            if(currentNum == 0)
-                currentNum = zero;
-            int leftGap = Math.abs(currentLeft - currentNum);
-            int rightGap = Math.abs(currentRight - currentNum);
-            if(leftGap/3 + leftGap%3 >rightGap/3 + rightGap%3){
-                sb.append("R");
-                currentRight = currentNum;
-                continue;
+            else{
+                int leftCnt = moveCount(leftPosition, number);
+                int rightCnt = moveCount(rightPosition, number);
+                
+                if(leftCnt < rightCnt){
+                    leftPosition = number;
+                    sb.append('L');
+                 }else if(leftCnt > rightCnt){
+                    rightPosition = number;
+                    sb.append('R');
+                }else{
+                    char h = hand.equals("right")? 'R' : 'L' ;
+                    sb.append(h);
+                    
+                    if(h == 'R')
+                        rightPosition = number;
+                    else
+                        leftPosition = number;
+                }
             }
-            if(leftGap/3 + leftGap%3 <rightGap/3 + rightGap%3){
-                sb.append("L");
-                currentLeft = currentNum;
-                continue;
-            }
-            //차이가 같다면 무슨손잡인지에 따라서
-            if(hand.equals("left")){
-                sb.append("L");
-                currentLeft = currentNum;
-                continue;
-            }
-            
-            sb.append("R");
-            currentRight= currentNum;
-            
         }
         
         return sb.toString();
+    }
+    
+    public int moveCount(int curNum, int targetNum){
+        if (targetNum == 0)
+            targetNum = 11;
+        if(curNum == 0)
+            curNum = 11;
+        
+        int number = targetNum - curNum;
+        if(number<0)
+            number *= -1;
+        
+        int move =  number / 3 + number % 3;
+        
+        return move;
     }
 }

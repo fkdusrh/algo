5580923b87ea20f0aff07c21f8310fe5cc55d39c
rayeon <fkdusrh@naver.com>
2026-02-19 21:55:37 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.37 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,49 +1,90 @@
-/*
-1. 3~15자 사이
-2. 소문자, 숫자, -, _, .
-    . : 처음과 끝, 연속 X
-    
-변환
-1. 대문자 -> 소문자
-2. 소문자, 숫자, -, _ , . 나머지는 제외
-3. .. -> . 치환
-4. 처음과 끝 . 제거
-5. 빈 문자열이면 a 대입
-6. 16자 이상이면, 처음15개만 남기기
-    ->마지막에 . 남으면 제거
-    
-7. 2자 이하라면, 마지막 문자를 3자 이상 될때까지 반복해서 붙이기
-*/
 import java.util.*;
 class Solution {
     public String solution(String new_id) {
-       
-        //1. 대문자 -> 소문자
-        new_id= new_id.toLowerCase();
-        //2. 소문자, 숫자, -, _ , . 나머지는 제외
-        new_id = new_id.replaceAll("[^a-z0-9-\\.\\_]","")
-            .replaceAll("\\.{2,}",".") //3.  ..연속 -> . 로 치환
-            .replaceAll("^\\.|\\.$","");//4. 처음과 끝 . 제거
-   
-        //5. 빈 문자열이면 a 대입
-        if(new_id.length()==0)
-            new_id="a";
-        //6. 15자 초과시 제거
-        if(new_id.length()>15)
-            new_id = new_id.substring(0,15);
-        //6.2 만약 뒤가 .이면 제거
-        new_id = new_id.replaceAll("\\.$","");
-        //7. 2자 이하라면, 마지막 문자를 3자 이상 될때까지 마지막 문자 반복해서 붙이기
         
-        if(new_id.length()<3){            
-            StringBuilder sb = new StringBuilder(new_id);
-            char last=sb.charAt(sb.length()-1); 
-            while(sb.length()<3){
-                 sb.append(last);
-             } 
-            new_id = sb.toString();
-        }
+        new_id = new_id.toLowerCase();
+        new_id = except(new_id);
+        new_id = convertSingle(new_id);
+        new_id = removeDotForEndOrStart(new_id);
+        new_id = addFirst(new_id);
+        new_id = cutLength(new_id);
+        new_id = addLength(new_id);
         
         return new_id;
     }
+    
+    public String except(String id){
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<id.length();i++){
+            char c = id.charAt(i);
+            
+            if(c == '.' || c== '_'|| c=='-' || (c-'0' >= 0 && c-'0'<=9 )|| (c-'a'>=0 && c-'a'<=25))
+                sb.append(c);
+        }
+            
+            return sb.toString();
+    }
+    
+    public String convertSingle(String id){
+        boolean successive = false;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<id.length();i++){
+            if(id.charAt(i) != '.') {
+                sb.append(id.charAt(i));
+                successive = false;
+            } else if(id.charAt(i) == '.' && !successive){
+                sb.append(id.charAt(i));
+                successive = true;
+            }
+        }
+
+        return sb.toString();
+    }
+    
+    public String removeDotForEndOrStart(String id){
+        if(id.startsWith("."))
+            id = id.substring(1);
+        if(id.endsWith("."))
+            id = id.substring(0, id.length() - 1);
+        
+        return id;
+    }
+    
+    public String addFirst(String id){
+        if(id.length() == 0)
+            return "a";
+        
+        return id;
+    }
+    
+    public String cutLength(String id){
+        if(id.length() >15){
+            id = id.substring(0,15);
+        }else{
+            return id;
+        }
+        
+        if(id.endsWith("."))
+            id = id.substring(0,14);
+        
+        return id;
+    }
+    
+    public String addLength(String id){
+        if(id.length()>2)
+            return id;
+        
+        StringBuilder sb = new StringBuilder();
+        char last = id.charAt(id.length()-1);
+        sb.append(id);
+        
+        while(sb.length()<=2){
+            sb.append(last);
+        }
+        
+        return sb.toString();
+    }
+    
 }

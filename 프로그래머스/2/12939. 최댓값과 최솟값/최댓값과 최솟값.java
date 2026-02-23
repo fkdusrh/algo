db9fda66eddf457391c91c914d25d8ee1c2c90bf
rayeon <fkdusrh@naver.com>
2026-02-23 19:49:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.39 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,6 +1,17 @@
+import java.util.*;
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        StringTokenizer st = new StringTokenizer(s);
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        while(st.hasMoreTokens()){
+            int v = Integer.parseInt(st.nextToken());
+            min = Math.min(min, v);
+            max = Math.max(max, v);
+        }
+        
+        return min+" "+max;
     }
 }

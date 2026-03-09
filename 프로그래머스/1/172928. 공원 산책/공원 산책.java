25d24d3cbb3353eb3df43842678d4515f0f65352
rayeon <fkdusrh@naver.com>
2026-03-09 16:16:06 +0900

    [level 1] Title: 공원 산책, Time: 0.33 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,16 +1,15 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        int[] answer = {};
-        int curRowIdx=0,curColIdx=0;
-        int[][] directions = {{-1,0}, {1,0},{0,-1},{0,1}};
+        int r=0,c=0;
         int width = park[0].length();
         int height = park.length;
-        Map<Character, Integer> hm = new HashMap<>();
-        hm.put('N',0);
-        hm.put('S',1);
-        hm.put('W',2);
-        hm.put('E',3);
+        
+        Map<Character, int[]> hm = new HashMap<>();
+        hm.put('N',new int[]{-1,0});
+        hm.put('S',new int[]{1,0});
+        hm.put('W',new int[]{0,-1});
+        hm.put('E',new int[]{0,1});
         
         char[][] map = new char[park.length][park[0].length()];
         
@@ -18,12 +17,12 @@ class Solution {
             String p = park[i];
             
             for(int j=0;j<width;j++){
-                char c = p.charAt(j);
-                map[i][j] = c;
+                char letter = p.charAt(j);
+                map[i][j] = letter;
                 
-                if(c == 'S'){
-                    curRowIdx = i;
-                    curColIdx = j;
+                if(letter == 'S'){
+                    r = i;
+                    c = j;
                 }
             }
         }
@@ -31,39 +30,35 @@ class Solution {
         for(int i=0;i<routes.length;i++){
             char op = routes[i].charAt(0);
             int dis = routes[i].charAt(2) - '0';
-            int[] direction = directions[hm.get(op)];
             
-            int nextRowIdx = curRowIdx + direction[0] * dis;
-            int nextColIdx = curColIdx + direction[1] * dis;
+            int dr = hm.get(op)[0];
+            int dc = hm.get(op)[1];
+            boolean canMove = true;
             
-            if(nextRowIdx <0 || nextRowIdx >= height)
-                continue;
-            if(nextColIdx <0 || nextColIdx >= width)
-                continue;
+            int nr = r;
+            int nc = c;
             
-            boolean available = true;
-            for(int j=curRowIdx; ; j += direction[0]){
-                if(map[j][curColIdx] == 'X')
-                    available = false;
-                if(j == nextRowIdx)
+            for(int j=0;j<dis;j++){
+                nr += dr;
+                nc += dc;
+                
+                if(nr < 0 || nc < 0 || nr >= height || nc >= width){
+                    canMove = false;
                     break;
+                }
+                
+                if(map[nr][nc] == 'X'){
+                    canMove = false;
+                    break;
+                }
             }
             
-            for(int j=curColIdx; ; j += direction[1]){
-                if(map[curRowIdx][j] == 'X')
-                    available = false;
-                if(j == nextColIdx)
-                    break;
+            if(canMove){
+                r += dr * dis;
+                c += dc * dis;
             }
-            
-            if(!available)
-                continue;
-            
-            curRowIdx += direction[0] * dis;
-            curColIdx += direction[1] * dis;
-            
         }
         
-        return new int[]{curRowIdx, curColIdx};
+        return new int[]{r, c};
     }
 }

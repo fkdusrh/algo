31d5ed55b0b8cd50834afd6acba0151c09b86a98
rayeon <fkdusrh@naver.com>
2026-09-09 14:13:39 +0900

    [level 1] Title: 모의고사, Time: 1.01 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    int[][] person = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5},{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+    
+    public int[] solution(int[] answers) {
+        int[] answer = new int[3];
+        int correctsCnt[] = new int[3];
+        
+        for(int i=0;i<answers.length;i++){
+            int ans = answers[i];
+            
+            for(int j=0;j<3;j++){
+                if(person[j][i%person[j].length] == ans)
+                    correctsCnt[j] += 1;
+            }
+        }
+        
+        int max = -1, size = 0;
+        
+        for(int i=0;i<correctsCnt.length;i++){            
+            if(correctsCnt[i] > max){
+                max = correctsCnt[i];
+            }
+        }
+        
+        for(int i=0;i<correctsCnt.length;i++){
+            if(correctsCnt[i] == max)
+                answer[size++] = i+1;
+        }
+        
+        return Arrays.copyOf(answer,size);
+    }
+}

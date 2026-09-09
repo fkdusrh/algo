b84e7cf44f2cde2061c652c30eb698c410d4a90f
rayeon <fkdusrh@naver.com>
2026-09-09 14:50:01 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.68 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,48 +2,29 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        int boardSize = board.length;
-        List<Stack<Integer>> ls = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
         
-        for(int i=0;i<board[0].length ; i++){//열
-            Stack<Integer> s = new Stack<>();
-            
-            for(int j=board.length-1;j>= 0;j--){//행
-                if(board[j][i] !=0)
-                    s.add(board[j][i]);
-            }
-            
-            ls.add(s);
-        }
-        
-        Stack<Integer> box = new Stack<>();
-          
         for(int move:moves){
-            if(ls.get(move-1).size()==0)
-                continue;
+            move --;
             
-            int grab = ls.get(move-1).pop();
-            if(box.size()==0){
-                box.push(grab);
-            } else {
-                if( box.peek() == grab){
-                    answer ++;
-                    box.pop();
-                }else{
-                    box.push(grab);
-                }
+            for(int i=0;i<board.length;i++){
+                int item = board[i][move];
+                
+                if(item > 0){
+                    if(st.size()>0 && st.peek() == item){
+                        st.pop();
+                        answer+=2;
+                    }else{
+                        st.push(item);
+                    }
+                    
+                    board[i][move] = 0;
+                    break;
+                 }
             }
+            
         }
         
-        return answer*2 ;
+        return answer;
     }
-    
-    public void print(Stack s){
-        System.out.print("바구니[");
-        for(int i=0;i<s.size();i++){
-            System.out.print(s.get(i)+" / ");
-        }
-        System.out.println("]");
-    }
-  
 }

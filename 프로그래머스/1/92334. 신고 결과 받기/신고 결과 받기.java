72e67089a63403de085af76df48547554181c483
rayeon <fkdusrh@naver.com>
2026-09-09 16:04:37 +0900

    [level 1] Title: 신고 결과 받기, Time: 106.69 ms, Memory: 191 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,35 +1,35 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String, Set<String>> reportsMap = new HashMap<>();
-        HashMap<String, Integer> indexMap = new HashMap<>();
         int[] answer = new int[id_list.length];
-
-
-        for (String r : report) {
-            String[] parts = r.split(" ");
-            String from = parts[0];
-            String to = parts[1];
-
-            reportsMap.putIfAbsent(to, new HashSet<>());
-            reportsMap.get(to).add(from);
+        HashMap<String, Set<String>> reportUser = new HashMap<>();
+        HashMap<String, Integer> mailCount = new HashMap<>();
+        
+        for(String rep: report){
+            String[] info = rep.split(" ");
+            String from= info[0];
+            String to = info[1];
+            
+            Set<String> set = reportUser.getOrDefault(to, new HashSet<>());
+            set.add(from);
+            reportUser.put(to, set);
         }
-        for (int i = 0; i < id_list.length; i++) {
-            indexMap.put(id_list[i], i);
-        }
-
-
-        for (Map.Entry<String, Set<String>> entry : reportsMap.entrySet()) {
-            Set<String> reporters = entry.getValue();
-            if (reporters.size() >= k) {
-                for (String reporter : reporters) {
-                    int idx = indexMap.get(reporter);
-                    answer[idx]++;
+        
+        for(String name: id_list){
+            Set<String> froms = reportUser.getOrDefault(name, new HashSet<>());
+            
+            if(froms.size() >= k){
+                for(String from: froms){
+                    int cnt = mailCount.getOrDefault(from, 0);
+                    mailCount.put(from, ++cnt);
                 }
             }
         }
-
+        
+        for(int i=0;i<id_list.length;i++){
+            String name = id_list[i];
+            answer[i] = mailCount.getOrDefault(name, 0);
+        }
         return answer;
     }
 }

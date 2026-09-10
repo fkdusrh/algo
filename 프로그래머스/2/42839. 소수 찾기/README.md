af304be8a18b4e4961319acbc556429d2b42074c
rayeon <fkdusrh@naver.com>
2026-09-10 20:09:07 +0900

    [level 2] Title: 소수 찾기, Time: 0.99 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 12.64 ms
+메모리: 74.7 MB, 시간: 0.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 14일 08:37:21
+2026년 09월 10일 20:09:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,41 +1,40 @@
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.*;
 class Solution {
-    private Set<Integer> numberSet = new HashSet<>();
-
-    private boolean isPrime(int num) {
-        if (num < 2) {
+    public int solution(String numbers) {
+        int[] nums = new int[numbers.length()];
+        for(int i=0;i<numbers.length();i++){
+            nums[i] = numbers.charAt(i)-'0';
+        }
+        return reputation(new boolean[10], nums, new HashSet<Integer>(), 0);
+    }
+    
+    private int reputation(boolean[] visit, int[] numbers, HashSet<Integer> set, int number){
+        if(number >1 && isPrime(number))
+            set.add(number);
+        
+        for(int i=0;i<numbers.length;i++){
+            if(visit[i])
+                continue;
+            
+            visit[i] = true;
+            reputation(visit,numbers,set, number *10 + numbers[i]);
+            visit[i]= false;
+        }
+        
+        return set.size();
+    }
+    
+    private boolean isPrime(int number){
+        if(number <2) 
             return false;
-        }
-        int limit = (int) Math.sqrt(num);
-        for (int i = 2; i <= limit; i++) {
-            if (num % i == 0) {
+        
+        for(int i=2;i*i<=number;i++){
+            if(number% i == 0)
                 return false;
-            }
         }
+        
         return true;
     }
-
-    private void generateCombinations(String combination, String remaining) {
-        if (!combination.isEmpty()) {
-            numberSet.add(Integer.parseInt(combination));
-        }
-        for (int i = 0; i < remaining.length(); i++) {
-            generateCombinations(combination + remaining.charAt(i), remaining.substring(0, i) + remaining.substring(i + 1));
-        }
-    }
-
-    public int solution(String numbers) {
-        numberSet.clear();
-        generateCombinations("", numbers);
-        
-        int count = 0;
-        for (int number : numberSet) {
-            if (isPrime(number)) {
-                count++;
-            }
-        }
-        return count;
-    }
+    
+    
 }

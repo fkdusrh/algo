b622ddc7ba07d6468e148f286e02d3aacc0be045
rayeon <fkdusrh@naver.com>
2026-09-10 14:11:57 +0900

    [level 1] Title: 달리기 경주, Time: 239.28 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,27 +1,30 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] ranking = new String[players.length];
-        Map<String, Integer> hm = new HashMap<>();
+        String[] answer = {};
+        HashMap<String, Integer> rankingMap = new HashMap<>();
+        String[] board = new String[players.length];
         
         for(int i=0;i<players.length;i++){
-            String player = players[i];
-            
-            hm.put(player, i+1);
-            ranking[i] = player; 
+            rankingMap.put(players[i], i + 1);
+            board[i] = players[i];
         }
         
-        for(String calledName:callings){
-            int calledRank = hm.get(calledName);
-            String frontName = ranking[calledRank-2];
-            
-            ranking[calledRank-1] = frontName;
-            ranking[calledRank -2 ] = calledName;
-            
-            hm.put(frontName, calledRank);
-            hm.put(calledName, calledRank -1 );
+        for(String calling:callings){
+            int beforeRank = rankingMap.get(calling);
+            String downName = change(board, calling, beforeRank);
+            rankingMap.put(calling, beforeRank - 1);
+            rankingMap.put(downName, rankingMap.get(downName)+1);
         }
         
-        return ranking;
+        return board;
+    }
+    
+    private String change(String[] ranking, String upName, int index){
+        String tmp = ranking[index-2];
+        ranking[index - 2 ] = upName;
+        ranking[index - 1] = tmp;
+        
+        return tmp;
     }
 }

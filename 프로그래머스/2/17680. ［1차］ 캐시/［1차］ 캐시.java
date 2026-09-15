f8eee455d4c95aa00c3792ada7b44295146bfb7b
rayeon <fkdusrh@naver.com>
2026-09-15 23:27:37 +0900

    [level 2] Title: [1차] 캐시, Time: 37.36 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,33 +1,59 @@
 import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        if(cacheSize == 0)
-            return 5 * cities.length;
-        
         int answer = 0;
-        List<String> ls = new ArrayList<>();
+        if(cacheSize == 0)
+            return cities.length * 5;
+        class Node{
+            String name;
+            int time;
+            
+            Node(String s, int n){
+                name = s;
+                time = n;
+            }
+        }
+        for(int i=0;i<cities.length;i++){
+            cities[i] = cities[i].toUpperCase();
+        }
         
+        List<Node> ls = new ArrayList<>();
+        int minIdx = 0;
+        int t = 0;
         for(String city:cities){
-            city = city.toLowerCase();
-            int index = -1;
+            t++;
+            boolean hit = false;
             
             for(int i=0;i<ls.size();i++){
-                if(city.equals(ls.get(i))){
-                    index = i;
+                Node n = ls.get(i); 
+                
+                if(n.name.equals(city)){
+                    n.time = t;
+                    ls.set(i, n);
+                    
+                    hit = true;
+                    answer += 1;
                     break;
                 }
+                
+                if(n.time < ls.get(minIdx).time){
+                    minIdx = i;
+                }
+            }
+            //캐시에 없으면 ls(minIdx) 제거하고, 새로 넣기 
+            if(!hit){
+                answer += 5;
+                
+                if(ls.size() < cacheSize){
+                    ls.add(new Node(city, t));
+                    continue;
+                }
+                
+                ls.remove(minIdx);
+                minIdx = 0;
+                ls.add(new Node(city, t));
             }
             
-            if(index  == -1){//캐시에 없음
-                if(ls.size() == cacheSize) //캐시가 꽉 찼을 때
-                    ls.remove(0);// 가장 오래 안쓰인 프레임 지운다.
-                answer += 5; 
-            }else{//캐시에 있음
-                ls.remove(index);
-                answer++;
-            }
-            
-            ls.add(city);
         }
         return answer;
     }
